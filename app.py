61d876c22643d224d25e6e2d0c16cbfed1771ee5
abdul-azim-khan <58114310+abdul-azim-khan@users.noreply.github.com>
2021-08-18 16:04:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open("rf.pickle","rb"))
+model = pickle.load(open("dt.pickle","rb"))
 
 
 
